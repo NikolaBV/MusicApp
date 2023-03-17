4d798d4b2708ba9fc1050ccf0f8a1ce4e9ee6d59
Your Name <you@example.com>
2023-03-17 11:09:23 +0200

    Register button functionality added

--- MusicApp/MusicApp/Register.Designer.cs
+++ MusicApp/MusicApp/Register.Designer.cs
@@ -31,74 +31,82 @@
             this.btnRegister = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtPassowordReg = new System.Windows.Forms.TextBox();
+            this.txtPasswordReg = new System.Windows.Forms.TextBox();
             this.txtUsernameReg = new System.Windows.Forms.TextBox();
             this.musicLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnRegister
             // 
-            this.btnRegister.Location = new System.Drawing.Point(210, 232);
+            this.btnRegister.Location = new System.Drawing.Point(158, 188);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.Size = new System.Drawing.Size(56, 19);
             this.btnRegister.TabIndex = 11;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.Register_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(95, 200);
+            this.label2.Location = new System.Drawing.Point(71, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 10;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 158);
+            this.label1.Location = new System.Drawing.Point(69, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Username";
             // 
-            // txtPassowordReg
+            // txtPasswordReg
             // 
-            this.txtPassowordReg.Location = new System.Drawing.Point(174, 195);
-            this.txtPassowordReg.Name = "txtPassowordReg";
-            this.txtPassowordReg.Size = new System.Drawing.Size(156, 22);
-            this.txtPassowordReg.TabIndex = 8;
+            this.txtPasswordReg.Location = new System.Drawing.Point(130, 158);
+            this.txtPasswordReg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPasswordReg.Name = "txtPasswordReg";
+            this.txtPasswordReg.Size = new System.Drawing.Size(118, 20);
+            this.txtPasswordReg.TabIndex = 8;
             // 
             // txtUsernameReg
             // 
-            this.txtUsernameReg.Location = new System.Drawing.Point(174, 153);
+            this.txtUsernameReg.Location = new System.Drawing.Point(130, 124);
+            this.txtUsernameReg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsernameReg.Name = "txtUsernameReg";
-            this.txtUsernameReg.Size = new System.Drawing.Size(156, 22);
+            this.txtUsernameReg.Size = new System.Drawing.Size(118, 20);
             this.txtUsernameReg.TabIndex = 7;
             // 
             // musicLabel
             // 
             this.musicLabel.AutoSize = true;
             this.musicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.musicLabel.Location = new System.Drawing.Point(183, 91);
+            this.musicLabel.Location = new System.Drawing.Point(137, 74);
+            this.musicLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.musicLabel.Name = "musicLabel";
-            this.musicLabel.Size = new System.Drawing.Size(128, 32);
+            this.musicLabel.Size = new System.Drawing.Size(101, 26);
             this.musicLabel.TabIndex = 6;
             this.musicLabel.Text = "Reigster";
             this.musicLabel.Click += new System.EventHandler(this.musicLabel_Click);
             // 
             // Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 357);
+            this.ClientSize = new System.Drawing.Size(346, 290);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtPassowordReg);
+            this.Controls.Add(this.txtPasswordReg);
             this.Controls.Add(this.txtUsernameReg);
             this.Controls.Add(this.musicLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Register";
             this.Text = "Register";
             this.Load += new System.EventHandler(this.Register_Load);
@@ -112,7 +120,7 @@
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtPassowordReg;
+        private System.Windows.Forms.TextBox txtPasswordReg;
         private System.Windows.Forms.TextBox txtUsernameReg;
         private System.Windows.Forms.Label musicLabel;
     }
--- MusicApp/MusicApp/Register.cs
+++ MusicApp/MusicApp/Register.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,5 +27,62 @@ namespace MusicApp
         {
 
         }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            if (CheckIfUserExists(txtUsernameReg.Text))
+            {
+                MessageBox.Show("There is existing user!");
+            }
+            else
+            {
+                RegisterUser(txtUsernameReg.Text, txtPasswordReg.Text);
+            }
+        }
+        private void RegisterUser(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MusicAppDb.mdf;Integrated Security=True;Connect Timeout=30;"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO [User] (Username, Password) VALUES (@Username, @Password)", connection);
+
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            MessageBox.Show("Succsesful register!");
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+        private bool CheckIfUserExists(string username)
+        {
+            bool userExists = false;
+
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MusicAppDb.mdf;Integrated Security=True;Connect Timeout=30;"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM [User] WHERE Username = @Username", connection);
+
+                command.Parameters.AddWithValue("@Username", username);
+
+                int count = (int)command.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    userExists = true;
+                }
+
+                connection.Close();
+            }
+
+            return userExists;
+        }
+
     }
 }
